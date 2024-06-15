46ed989d71d47fc87561ead648ad29a08d8660f0
Jacky Chen <chenjacky32@gmail.com>
2024-06-15 16:53:48 +0700

    feat: tambah fitur addRatings

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,6 +9,7 @@ import AddBookmark from '../handler/Bookmarks/handlerAddBookmark.js';
 import GetDestBookmark from '../handler/Bookmarks/handlerGetDestBookmark.js';
 import GetDestUnBookmarked from '../handler/Bookmarks/handlerGetDestUnBookmark.js';
 import UnBookmark from '../handler/Bookmarks/handlerUnBookmark.js';
+import AddRatings from '../handler/Ratings/AddRatings.js';
 
 const Routes = [
   {
@@ -65,6 +66,11 @@ const Routes = [
     method: 'GET',
     path: '/destinations/unbookmarked',
     handler: GetDestUnBookmarked,
+  },
+  {
+    method: 'POST',
+    path: '/destinations/{dest_id}/ratings',
+    handler: AddRatings,
   },
 ];
 
